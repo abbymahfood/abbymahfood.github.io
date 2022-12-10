284cb535ce61f0a6a6217f86d35df58c1dc85889
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 17:10:54 -0500

    adding dynamic data to resume page

--- templates/resume-template.js
+++ templates/resume-template.js
@@ -4,34 +4,37 @@ const ResumeTemplate = `
         <h3 id="top">Software Skills</h3>
         <article>
             <h4>Languages</h4>
-            <div style="display: flex;">
-                <p v-for = "language in resumeInfo.Languages" :key = "resumeInfo.Languages">{{language}}</p>
+            <div class="skillset">
+                <p v-for = "language in resumeInfo.Languages" :key = "resumeInfo.Languages">{{language}} </p>
             </div>
         </article>
         <article>
             <h4>Platforms</h4>
-            <div style="display: flex;">
-                <p v-for = "platform in resumeInfo.Platforms" :key = "resumeInfo.Platforms">{{platform}}</p>
+            <div class="skillset">
+                <p v-for = "platform in resumeInfo.Platforms" :key = "resumeInfo.Platforms">{{platform}} </p>
             </div>
         </article>
         <article>
             <h4>Frameworks</h4>
-            <div style="display: flex;">
-                <p v-for = "framework in resumeInfo.Frameworks" :key = "resumeInfo.Frameworks">{{framework}}</p>
+            <div class="skillset">
+                <p v-for = "framework in resumeInfo.Frameworks" :key = "resumeInfo.Frameworks">{{framework}} </p>
             </div>
         </article>
         <article>
             <h4>Programs</h4>
-            <div style="display: flex;">
-                <p v-for = "program in resumeInfo.Programs" :key = "resumeInfo.Programs">{{program}}</p>
+            <div class="skillset">
+                <p v-for = "program in resumeInfo.Programs" :key = "resumeInfo.Programs">{{program}} </p>
             </div>
         </article>
     </section>
     <section v-bind:resumeInfo = "resumeInfo">
         <h3 id="top">Experience</h3>
         <article v-for = "experience in resumeInfo.Experience" :key = "resumeInfo.Experience">
-            <h4>{{experience.Company}}</h4>
-            <p>{{experience.Title}}</p>
+            <div class="experience-header">
+                <h4 style="float: left;>{{experience.Company}}</h4>
+                <p style="font-style:italic; float: right;">{{experience.Period}}</p>
+            </div>
+            <p style="font-style:italic;">{{experience.Title}}</p>
             <ul>
                 <li v-for = "resp in experience.Responsibilities" :key = "experience.Responsibilities">{{resp}}</li>
             </ul>
