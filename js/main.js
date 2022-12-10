763fbf549678b61a70cab5a8633d88eac6c6d8a8
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 11:53:55 -0500

    reordering components

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
 import {
-    Navbar
-  } from './../components/navbar.js'
-  
-  import {
-    MainTemplate
-  } from './../templates/main-template.js'
+  Navbar
+} from './../components/navbar.js'
 
-  import {
-    AboutTemplate
-  } from './../templates/about-template.js'
+import {
+  MainTemplate
+} from './../templates/main-template.js'
+
+import {
+  AboutTemplate
+} from './../templates/about-template.js'
+
+const router = new VueRouter({
+  routes: [{
+    path: '/about',
+    component: AboutTemplate,
+    name: "About Page"
+  }]
+})
 
 new Vue({
-    el: '#app', // This should be the same as your <div id=""> from earlier.
-    components: {
-      'navbar': Navbar
-    },
-    router,
-    template: MainTemplate
-  })
+  el: '#app', // This should be the same as your <div id=""> from earlier.
+  components: {
+    'navbar': Navbar
+  },
+  router,
+  template: MainTemplate
+})
 
-  Vue.use(VueRouter)
+Vue.use(VueRouter)
 
-  const router = new VueRouter({
-    routes: [{
-      path: '/about',
-      component: AboutTemplate,
-      name: "About Page"
-    }]
-  })
+
 
