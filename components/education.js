c5aa35cafde9748ad25e3643175800d224c1cb7c
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 13:53:25 -0500

    still working on scrolling to top

--- components/education.js
+++ components/education.js
@@ -4,8 +4,11 @@ const Education = {
   template: EducationTemplate,
   methods: {
     scrollMeTo(refName) {
+      console.log(refName);
       var element = this.$refs[refName];
+      console.log(element);
       var top = element.offsetTop;
+      console.log(top);
       window.scrollTo(0, top);
     }
   }
