bc426c52cf7b81cd9372b36163cae2a3ad867501
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 16:59:10 -0500

    adding dynamic data to resume page

--- templates/resume-template.js
+++ templates/resume-template.js
@@ -4,25 +4,25 @@ const ResumeTemplate = `
         <h3 id="top">Software Skills</h3>
         <article>
             <h4>Languages</h4>
-            <div style="display: flex; justify-content: space-between;">
+            <div style="display: flex;">
                 <p v-for = "language in resumeInfo.Languages" :key = "resumeInfo.Languages">{{language}}</p>
             </div>
         </article>
         <article>
             <h4>Platforms</h4>
-            <div style="display: flex; justify-content: space-between;">
+            <div style="display: flex;">
                 <p v-for = "platform in resumeInfo.Platforms" :key = "resumeInfo.Platforms">{{platform}}</p>
             </div>
         </article>
         <article>
             <h4>Frameworks</h4>
-            <div style="display: flex; justify-content: space-between;">
+            <div style="display: flex;">
                 <p v-for = "framework in resumeInfo.Frameworks" :key = "resumeInfo.Frameworks">{{framework}}</p>
             </div>
         </article>
         <article>
             <h4>Programs</h4>
-            <div style="display: flex; justify-content: space-between;">
+            <div style="display: flex;">
                 <p v-for = "program in resumeInfo.Programs" :key = "resumeInfo.Programs">{{program}}</p>
             </div>
         </article>
@@ -30,7 +30,11 @@ const ResumeTemplate = `
     <section v-bind:resumeInfo = "resumeInfo">
         <h3 id="top">Experience</h3>
         <article v-for = "experience in resumeInfo.Experience" :key = "resumeInfo.Experience">
-            <h4>{{experience.Company}} - {{experience.Title}}</h4>
+            <h4>{{experience.Company}}</h4>
+            <p>{{experience.Title}}</p>
+            <ul>
+                <li v-for = "resp in experience.Responsibilities" :key = "experience.Responsibilities">{{resp}}</li>
+            </ul>
         </article> 
     </section>
     <a id="top-link" @click="scrollMeTo()">Top</a>
