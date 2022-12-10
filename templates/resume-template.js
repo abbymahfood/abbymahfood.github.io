e9e92ec6e232d6969d073a9e512ee5119a88404d
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 17:53:11 -0500

    adding dynamic data to resume page

--- templates/resume-template.js
+++ templates/resume-template.js
@@ -1,41 +1,6 @@
 const ResumeTemplate = `
 <div class="main">
     <section v-bind:resumeInfo = "resumeInfo">
-        <h3 id="top">Software Skills</h3>
-        <article v-if="resumeInfo.Languages">
-            <h4>Languages</h4>
-            <div class="skillset">
-                <ul>
-                    <li v-for = "language in resumeInfo.Languages" :key = "resumeInfo.Languages">{{language}}</li>
-                </ul>
-            </div>
-        </article>
-        <article v-if="resumeInfo.Platforms">
-            <h4>Platforms</h4>
-            <div class="skillset">
-                <ul>
-                    <li v-for = "platform in resumeInfo.Platforms" :key = "resumeInfo.Platforms">{{platform}}</li>
-                </ul>
-            </div>
-        </article>
-        <article v-if="resumeInfo.Frameworks">
-            <h4>Frameworks</h4>
-            <div class="skillset">
-                <ul>
-                    <li v-for = "framework in resumeInfo.Frameworks" :key = "resumeInfo.Frameworks">{{framework}}</li>
-                </ul>
-            </div>
-        </article>
-        <article v-if="resumeInfo.Programs"> 
-            <h4>Programs</h4>
-            <div class="skillset">
-                <ul>
-                    <li v-for = "program in resumeInfo.Programs" :key = "resumeInfo.Programs">{{program}}</li>
-                </ul>
-            </div>
-        </article>
-    </section>
-    <section v-bind:resumeInfo = "resumeInfo">
         <h3 id="top">Experience</h3>
         <article v-if="resumeInfo.Experience" v-for = "experience in resumeInfo.Experience" :key = "resumeInfo.Experience">
             <div class="experience-header">
@@ -48,6 +13,54 @@ const ResumeTemplate = `
             </ul>
         </article> 
     </section>
+    <section v-bind:resumeInfo = "resumeInfo">
+        <h3 id="top">Education</h3>
+        <div style="display: flex;">
+            <article v-if="resumeInfo.Education" v-for = "education in resumeInfo.Education" :key = "resumeInfo.Education">
+                <div class="education-header">
+                    <h4 style="width: 50%;">{{education.Name}}</h4>
+                    <p style="width: 50%; font-style: italic; margin: 0px; text-align: right;">{{education.Graduation}}</p>
+                </div>
+                <p style="font-style:italic; margin: 0px;">{{education.Major}}</p>
+                <p style="margin: 0px;">{{education.DegreeType}}</p>
+            </article> 
+        </div>
+    </section>
+    <section v-bind:resumeInfo = "resumeInfo">
+    <h3 id="top">Software Skills</h3>
+    <article v-if="resumeInfo.Languages">
+        <h4>Languages</h4>
+        <div class="skillset">
+            <ul>
+                <li v-for = "language in resumeInfo.Languages" :key = "resumeInfo.Languages">{{language}}</li>
+            </ul>
+        </div>
+    </article>
+    <article v-if="resumeInfo.Platforms">
+        <h4>Platforms</h4>
+        <div class="skillset">
+            <ul>
+                <li v-for = "platform in resumeInfo.Platforms" :key = "resumeInfo.Platforms">{{platform}}</li>
+            </ul>
+        </div>
+    </article>
+    <article v-if="resumeInfo.Frameworks">
+        <h4>Frameworks</h4>
+        <div class="skillset">
+            <ul>
+                <li v-for = "framework in resumeInfo.Frameworks" :key = "resumeInfo.Frameworks">{{framework}}</li>
+            </ul>
+        </div>
+    </article>
+    <article v-if="resumeInfo.Programs"> 
+        <h4>Programs</h4>
+        <div class="skillset">
+            <ul>
+                <li v-for = "program in resumeInfo.Programs" :key = "resumeInfo.Programs">{{program}}</li>
+            </ul>
+        </div>
+    </article>
+</section>
     <a id="top-link" @click="scrollMeTo()">Top</a>
 </div>
 `
