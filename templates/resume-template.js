9ec3f3eed3a1d0666bcb1160143cde9a05969724
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 16:52:32 -0500

    adding dynamic data to resume page

--- templates/resume-template.js
+++ templates/resume-template.js
@@ -4,10 +4,36 @@ const ResumeTemplate = `
         <h3 id="top">Software Skills</h3>
         <article>
             <h4>Languages</h4>
-            <p v-for = "language in resumeInfo.Languages" :key = "resumeInfo.Languages">{{language}}</p>
+            <div style="display: flex; justify-content: space-between;">
+                <p v-for = "language in resumeInfo.Languages" :key = "resumeInfo.Languages">{{language}}</p>
+            </div>
         </article>
-        <a id="top-link" @click="scrollMeTo()">Top</a>
+        <article>
+            <h4>Platforms</h4>
+            <div style="display: flex; justify-content: space-between;">
+                <p v-for = "platform in resumeInfo.Platforms" :key = "resumeInfo.Platforms">{{platform}}</p>
+            </div>
+        </article>
+        <article>
+            <h4>Frameworks</h4>
+            <div style="display: flex; justify-content: space-between;">
+                <p v-for = "framework in resumeInfo.Frameworks" :key = "resumeInfo.Frameworks">{{framework}}</p>
+            </div>
+        </article>
+        <article>
+            <h4>Programs</h4>
+            <div style="display: flex; justify-content: space-between;">
+                <p v-for = "program in resumeInfo.Programs" :key = "resumeInfo.Programs">{{program}}</p>
+            </div>
+        </article>
     </section>
+    <section v-bind:resumeInfo = "resumeInfo">
+        <h3 id="top">Experience</h3>
+        <article v-for = "experience in resumeInfo.Experience" :key = "resumeInfo.Experience">
+            <h4>{{experience.Company}} - {{experience.Title}}</h4>
+        </article> 
+    </section>
+    <a id="top-link" @click="scrollMeTo()">Top</a>
 </div>
 `
 
