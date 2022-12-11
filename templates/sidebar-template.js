49364c3835d3d82d818cd53a1ec8756e6451da8a
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 21:19:04 -0500

    minor adjustments

--- templates/sidebar-template.js
+++ templates/sidebar-template.js
@@ -2,7 +2,7 @@ const SidebarTemplate = `<aside class="sidebar">
 <h2>Abby Mahfood</h2>
 <img src="imgs/abby/Me.jpg" alt="Picture of Abby in Savannah, GA enjoying iced coffee.">
 <p class="contact-info">
-  <a href="mailto:amahfood@bu.edu">
+  <a target="_blank" rel="noopener noreferrer" href="mailto:amahfood@bu.edu">
     Email
   </a>
   <a href="https://www.linkedin.com/in/abby-mahfood-40b24ba5">
