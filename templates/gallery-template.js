56cbfad0c27c382038b85ddebed29ac1e17dc219
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 20:26:36 -0500

    working on gallery

--- templates/gallery-template.js
+++ templates/gallery-template.js
@@ -2,17 +2,18 @@ const GalleryTemplate = `
 <div class="main">
     <h3 id="formTitle">Gallery</h3>
     <form name="galleryForm" id="galleryForm" action="javascript:void(0);" novalidate @onsubmit="return processForm();">
-    <fieldset class="outerFieldset">
-        <fieldset class="innerFieldset">
-            <input class="textbox" type="text" name="gallerySearch" id="gallerySearch" placeholder="Search Gallery" required autofocus>
-            <div id="gallerySearchValidationMsg" class="validationErrorMsg"></div>
+        <fieldset class="outerFieldset">
+            <fieldset class="innerFieldset">
+                <input class="textbox" type="text" name="gallerySearch" id="gallerySearch" placeholder="Search Gallery" required autofocus>
+                <div id="gallerySearchValidationMsg" class="validationErrorMsg"></div>
+            </fieldset>
+            <input type="submit" id="submit" value="Submit">
         </fieldset>
-        <input type="submit" id="submit" value="Submit">
-    </fieldset>
+    </form>
 
     <section v-bind:galleryImages = "galleryImages">
         <article class="gallery-image" v-for = "image in galleryImages" :key = "galleryImages">
-            <img :src="../imgs/image.Path" :alt="image.Description" />
+            <img :src="'../imgs/' + image.Path" :alt="image.Description" >
         </article>
     </section>
 </div>
