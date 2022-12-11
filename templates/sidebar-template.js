1ffda18e74632e35f8ea9479c2e512ad8946626e
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 21:25:00 -0500

    fixing sidebar links

--- templates/sidebar-template.js
+++ templates/sidebar-template.js
@@ -2,14 +2,13 @@ const SidebarTemplate = `<aside class="sidebar">
 <h2>Abby Mahfood</h2>
 <img src="imgs/abby/Me.jpg" alt="Picture of Abby in Savannah, GA enjoying iced coffee.">
 <p class="contact-info">
-  <a target="_blank" rel="noopener noreferrer" href="mailto:amahfood@bu.edu">
+  <a href="mailto:amahfood@bu.edu">
     Email
   </a>
-  <a href="https://www.linkedin.com/in/abby-mahfood-40b24ba5">
+  <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/abby-mahfood-40b24ba5">
     LinkedIn
   </a>
 </p>
 </aside>
 `
-
 export { SidebarTemplate }
