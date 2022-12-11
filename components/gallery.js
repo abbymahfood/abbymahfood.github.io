a96f1ab29e5fb5ce440baa34470be117ebf76df2
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 21:08:51 -0500

    tailoring search results

--- components/gallery.js
+++ components/gallery.js
@@ -34,28 +34,28 @@ const Gallery = {
       scrollArea.scrollTo(0, 0);
     },
     onSubmit() {
-      console.log("In process form");
-      let searchText = document.getElementById("gallerySearch").value;
-      console.log(searchText);
+      let searchText = document.getElementById("gallerySearch").value.toLowerCase();
       // GET request using fetch with error handling
       fetch("https://abbymahfood.github.io/jsons/images.json")
       .then(async response => {
         const data = await response.json();
-
         // check for error response
         if (!response.ok) {
           // get error message from body or default to response statusText
           const error = (data && data.message) || response.statusText;
           return Promise.reject(error);
         }
-
-        var newArray = data.Images.filter(function (el) {
-          return el.Tags.includes(searchText) ||
-                el.Description.includes(gallerySearch);
-        });
-
-        this.galleryImages = newArray;
-        console.log(data.galleryImages);
+        if (searchText != "" && searchText != null)
+        {
+          var newArray = data.Images.filter(function (el) {
+            return el.Tags.includes(searchText) ||
+                  el.Description.toLowerCase().includes(gallerySearch);
+          });
+          this.galleryImages = newArray;
+        }
+        else {
+          this.galleryImages = data.Images;
+        }
       })
       .catch(error => {
         this.errorMessage = error;
