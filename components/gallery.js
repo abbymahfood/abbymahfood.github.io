02dbca83146445b25ebe79ae2fa7e2f1c454789c
Abby Mahfood <abby.mahfood@gmail.com>
2022-12-10 20:19:07 -0500

    working on gallery

--- components/gallery.js
+++ components/gallery.js
@@ -8,6 +8,25 @@ const Gallery = {
       errorMessage: null
     };
   },
+  created() {
+    // GET request using fetch with error handling
+    fetch("https://abbymahfood.github.io/jsons/images.json")
+    .then(async response => {
+      const data = await response.json();
+
+      // check for error response
+      if (!response.ok) {
+        // get error message from body or default to response statusText
+        const error = (data && data.message) || response.statusText;
+        return Promise.reject(error);
+      }
+      this.galleryImages = data.Images;
+    })
+    .catch(error => {
+      this.errorMessage = error;
+      console.error("There was an error!", error);
+    });
+  },
   methods: {
     scrollMeTo() {
       var scrollArea = document.getElementsByClassName("main")[0];
--- templates/gallery-template.js
+++ templates/gallery-template.js
@@ -1,7 +1,7 @@
 const GalleryTemplate = `
 <div class="main">
     <h3 id="formTitle">Gallery</h3>
-    <form name="galleryForm" id="galleryForm" action="javascript:void(0);" novalidate onsubmit="return processForm();">
+    <form name="galleryForm" id="galleryForm" action="javascript:void(0);" novalidate @onsubmit="return processForm();">
     <fieldset class="outerFieldset">
         <fieldset class="innerFieldset">
             <input class="textbox" type="text" name="gallerySearch" id="gallerySearch" placeholder="Search Gallery" required autofocus>
@@ -9,6 +9,12 @@ const GalleryTemplate = `
         </fieldset>
         <input type="submit" id="submit" value="Submit">
     </fieldset>
+
+    <section v-bind:galleryImages = "galleryImages">
+        <article class="gallery-image" v-for = "image in galleryImages" :key = "galleryImages">
+            <img :src="../image.Path" :alt="image.Description" />
+        </article>
+    </section>
 </div>
 `
 export { GalleryTemplate }
